8c6c694176e0cb4cd0c5ce120b27ddf60fb6e5cc
PejoTech <peter.weber.bern@gmail.com>
2015-11-28 01:27:42 +0100

    Base type for view

---
+++ WorkEffect.Website/Types/AppUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkEffect.Website.Types
+{
+    public class AppUser : ClaimsPrincipal
+    {
+        public AppUser(ClaimsPrincipal principal)
+            : base(principal)
+        {
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.FindFirst(ClaimTypes.Name).Value;
+            }
+        }
+
+        public string Country
+        {
+            get
+            {
+                return this.FindFirst(ClaimTypes.Country).Value;
+            }
+        }
+    }
+}
---
+++ WorkEffect.Website/Views/AppViewPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WorkEffect.Website.Types;
+
+namespace WorkEffect.Website.Views
+{
+    public abstract class AppViewPage<TModel> : WebViewPage<TModel>
+    {
+        protected AppUser CurrentUser
+        {
+            get
+            {
+                return new AppUser(this.User as ClaimsPrincipal);
+            }
+        }
+    }
+
+    public abstract class AppViewPage : AppViewPage<dynamic>
+    {
+    }
+}
