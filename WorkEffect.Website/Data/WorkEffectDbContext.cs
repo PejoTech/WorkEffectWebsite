2f98a0fa991f6afe20e5a7ff5422e73d0edade27
PejoTech <peter.weber.bern@gmail.com>
2015-11-28 23:25:00 +0100

    Add CMS types

--- WorkEffect.Website/Data/WorkEffectDbContext.cs
+++ WorkEffect.Website/Data/WorkEffectDbContext.cs
@@ -5,11 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.EntityFramework;
+using WorkEffect.Website.Types;
 
 namespace WorkEffect.Website.Data
 {
     class WorkEffectDbContext : IdentityDbContext<AppUser>
     {
+        public DbSet<CmsPage> Pages{ get; set; }
+        public DbSet<CmsPart> Parts { get; set; }
+        public DbSet<CmsContent> Contents { get; set; }
+
         public WorkEffectDbContext()
             : base("DefaultConnection")
         {
