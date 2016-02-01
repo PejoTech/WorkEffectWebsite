262140f4022e5ec0225be7cdce7e16b2ffc426fd
PejoTech <peter.weber.bern@gmail.com>
2016-02-01 18:13:56 +0100

    Add Section and Layout Controller

---
+++ WorkEffect.Website/Controllers/LayoutController.cs
@@ -0,0 +1,121 @@
+﻿using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WorkEffect.Website.Models;
+
+namespace WorkEffect.Website.Controllers
+{
+    public class LayoutController : BaseEntityController<Layout>
+    {
+        //// GET: Layout
+        //public async Task<ActionResult> Index()
+        //{
+        //    return System.Web.UI.WebControls.View(await Context.Layouts.ToListAsync());
+        //}
+
+        //// GET: Layout/Details/5
+        //public async Task<ActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Layout layout = await Context.Layouts.FindAsync(id);
+        //    if (layout == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(layout);
+        //}
+
+        //// GET: Layout/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Layout/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Create([Bind(Include = "Id,Name,HtmlContainer")] Layout layout)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        Context.Layouts.Add(layout);
+        //        await Context.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(layout);
+        //}
+
+        //// GET: Layout/Edit/5
+        //public async Task<ActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Layout layout = await Context.Layouts.FindAsync(id);
+        //    if (layout == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(layout);
+        //}
+
+        //// POST: Layout/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Edit([Bind(Include = "Id,Name,HtmlContainer")] Layout layout)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        Context.Entry(layout).State = EntityState.Modified;
+        //        await Context.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(layout);
+        //}
+
+        //// GET: Layout/Delete/5
+        //public async Task<ActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Layout layout = await Context.Layouts.FindAsync(id);
+        //    if (layout == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(layout);
+        //}
+
+        //// POST: Layout/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> DeleteConfirmed(int id)
+        //{
+        //    Layout layout = await Context.Layouts.FindAsync(id);
+        //    Context.Layouts.Remove(layout);
+        //    await Context.SaveChangesAsync();
+        //    return RedirectToAction("Index");
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        Context.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
---
+++ WorkEffect.Website/Controllers/SectionController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WorkEffect.Website.Controllers;
+using WorkEffect.Website.Models;
+
+namespace ControllerGenerator.Controllers
+{
+    public class SectionController : BaseEntityController<Section>
+    {
+        //// GET: Section
+        //public async Task<ActionResult> Index()
+        //{
+        //    var sections = Context.Sections.Include(s => s.Layout);
+        //    return View(await sections.ToListAsync());
+        //}
+
+        //// GET: Section/Details/5
+        //public async Task<ActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Section section = await Context.Sections.FindAsync(id);
+        //    if (section == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(section);
+        //}
+
+        //// GET: Section/Create
+        //public ActionResult Create()
+        //{
+        //    ViewBag.LayoutId = new SelectList(Context.Layouts, "Id", "Name");
+        //    return View();
+        //}
+
+        //// POST: Section/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Create([Bind(Include = "Id,Name,Order,Content,Image,LayoutId")] Section section)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        Context.Sections.Add(section);
+        //        await Context.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    ViewBag.LayoutId = new SelectList(Context.Layouts, "Id", "Name", section.LayoutId);
+        //    return View(section);
+        //}
+
+        //// GET: Section/Edit/5
+        //public async Task<ActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Section section = await Context.Sections.FindAsync(id);
+        //    if (section == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.LayoutId = new SelectList(Context.Layouts, "Id", "Name", section.LayoutId);
+        //    return View(section);
+        //}
+
+        //// POST: Section/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Order,Content,Image,LayoutId")] Section section)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        Context.Entry(section).State = EntityState.Modified;
+        //        await Context.SaveChangesAsync();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.LayoutId = new SelectList(Context.Layouts, "Id", "Name", section.LayoutId);
+        //    return View(section);
+        //}
+
+        //// GET: Section/Delete/5
+        //public async Task<ActionResult> Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Section section = await Context.Sections.FindAsync(id);
+        //    if (section == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(section);
+        //}
+
+        //// POST: Section/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> DeleteConfirmed(int id)
+        //{
+        //    Section section = await Context.Sections.FindAsync(id);
+        //    Context.Sections.Remove(section);
+        //    await Context.SaveChangesAsync();
+        //    return RedirectToAction("Index");
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        Context.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+    }
+}
