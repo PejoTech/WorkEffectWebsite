6b27e74995d75c9b7b8acafdfcfa336dc91e0fba
PejoTech <peter.weber.bern@gmail.com>
2016-06-07 02:32:10 +0200

    Clean from scratch :) again :)

---
+++ WorkEffect.Website/Controllers/WorkEffectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkEffect.Website.Controllers
+{
+    public class WorkEffectController : Controller
+    {
+        // GET: WorkEffect
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WorkEffect.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WorkEffect.Website.MvcApplication" Language="C#" %>
